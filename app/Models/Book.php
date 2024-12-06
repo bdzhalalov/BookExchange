71101f02cab48469383da92767d7ac7954207158
bahadzhalalov <bahadzhalalov@gmail.com>
2024-12-06 23:50:34 +0500

    add models and migrations for books and requests

---
+++ app/Models/Book.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'author',
+        'genre',
+        'condition',
+        'cover_image',
+        'is_available',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function requests(): HasMany
+    {
+        return $this->hasMany(Request::class, 'book_id', 'id');
+    }
+}
