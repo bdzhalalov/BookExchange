bcfe6513bfc4beadb03422ad33c4701ebe67d3b6
bahadzhalalov <bahadzhalalov@gmail.com>
2024-12-18 17:35:07 +0500

    improve tests for books endpoints

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -2,6 +2,8 @@
 
 namespace Tests;
 
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 
@@ -9,9 +11,16 @@ class ApiTestCase extends TestCase
 {
     use DatabaseMigrations, WithFaker;
 
+    protected $user;
+
+    protected $books;
+
     public function setUp(): void
     {
         parent::setUp();
         $this->artisan('migrate');
+
+        $this->user = User::factory()->unverified()->create();
+        $this->books = Book::factory(5)->for($this->user)->create();
     }
 }
--- tests/Feature/BookApiTest.php
+++ tests/Feature/BookApiTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\Book;
-use App\Models\User;
 use Tests\ApiTestCase;
 
 class BookApiTest extends ApiTestCase
@@ -11,44 +9,43 @@ class BookApiTest extends ApiTestCase
     /** @test */
     public function test_can_get_all_books()
     {
-        $books = Book::factory(10)->create();
-        $response = $this->actingAs(User::factory()->create())
+        $response = $this->actingAs($this->user)
             ->getJson('/api/books');
 
         $response->assertStatus(200)
-            ->assertJsonCount(10, '');
+            ->assertJsonCount(5);
     }
 
     /** @test */
     public function test_returns_unauthorized_if_user_not_logged_in_when_getting_books()
     {
         $response = $this->getJson('/api/books');
-        $response->assertStatus(401); // Unauthorized
+        $response->assertStatus(401);
     }
 
     /** @test */
     public function test_can_get_a_single_book()
     {
-        $book = Book::factory()->create();
-        $response = $this->actingAs(User::factory()->create())
-            ->getJson("/api/books/{$book->id}");
+        $bookId = $this->books[0]->id;
+
+        $response = $this->actingAs($this->user)
+            ->getJson("/api/books/{$bookId}");
 
         $response->assertStatus(200)
-            ->assertJsonPath('data.id', $book->id);
+            ->assertJsonIsArray();
     }
 
     /** @test */
     public function test_returns_unauthorized_when_trying_to_get_a_single_book_without_login()
     {
-        $book = Book::factory()->create();
-        $response = $this->getJson("/api/books/{$book->id}");
+        $response = $this->getJson("/api/books/{$this->books[0]->id}");
         $response->assertStatus(401);
     }
 
     /** @test */
     public function test_returns_404_for_nonexistent_book()
     {
-        $response = $this->actingAs(User::factory()->create())
+        $response = $this->actingAs($this->user)
             ->getJson('/api/books/345');
 
         $response->assertStatus(404);
